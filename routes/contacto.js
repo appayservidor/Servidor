14681e8c7e4aa89baff14c2ec30de0a1d5761b27
Plr1993 <pablolope93@gmail.com>
2017-02-15 18:33:04 +0100

    CorreccionContacto
    Me faltaba conecto

--- routes/contacto.js
+++ routes/contacto.js
@@ -14,33 +14,36 @@ var mySecretKey=process.env.JWT_SECRETKEY;
 
 //Peticion de cambio de contraseña. Se llamará cuando alguien no recuerde su contraseña
 router.post('/',function(req,res){
-    var email="pablolope93@gmail.com"; //PARA HACER LAS PRUEBAS
-    var Texto = connection.escape(req.body.texto);
-    var Correo = connection.escape(req.body.correo);
-    var Nombre = connection.escape(req.body.nombre);
-    var smtpTransport = nodemailer.createTransport("SMTP",{
-        service: "gmail",
-        auth: {
-            user: process.env.GMAIL_USER,
-            pass: process.env.GMAIL_PASS
-        }
-    });
-    var htmlcorreo=emailhtml(Texto, Correo, Nombre); 
-    var mailOptions = {
-        from: "<appayoficial@gmail.com>", // sender address
-        to: email, //
-        subject: "Confirmar registro Appay", // Subject line
-        html: htmlcorreo
-        
-    }		
-    smtpTransport.sendMail(mailOptions, function(error, response){
-        if(error){
-            console.log(error);
-            res.status(300).json(error);
-        }else{
-            console.log("Correo enviado");
-            res.status(200).json("Todo bien todo correcto");
-        }
+    db.getConnection(function(err, connection) {
+        if (err) throw err;
+        var email="pablolope93@gmail.com"; //PARA HACER LAS PRUEBAS
+        var Texto = connection.escape(req.body.texto);
+        var Correo = connection.escape(req.body.correo);
+        var Nombre = connection.escape(req.body.nombre);
+        var smtpTransport = nodemailer.createTransport("SMTP",{
+            service: "gmail",
+            auth: {
+                user: process.env.GMAIL_USER,
+                pass: process.env.GMAIL_PASS
+            }
+        });
+        var htmlcorreo=emailhtml(Texto, Correo, Nombre); 
+        var mailOptions = {
+            from: "<appayoficial@gmail.com>", // sender address
+            to: email, //
+            subject: "Confirmar registro Appay", // Subject line
+            html: htmlcorreo
+            
+        }		
+        smtpTransport.sendMail(mailOptions, function(error, response){
+            if(error){
+                console.log(error);
+                res.status(300).json(error);
+            }else{
+                console.log("Correo enviado");
+                res.status(200).json("Todo bien todo correcto");
+            }
+        });
     });
 });
 
