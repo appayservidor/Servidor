fa320f088eaea0a3e96ffc884f6256aeedaf6a2a
Plr1993 <pablolope93@gmail.com>
2017-03-20 09:17:53 +0100

    Add database

--- helpers/database.js
+++ helpers/database.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+/*var mysql = require('mysql');
 var db=null;
 
 module.exports = function () {
@@ -16,5 +16,22 @@ module.exports = function () {
         });
     }
     return db;
+};*/
+var mysql = require('mysql');
+var db=null;
+
+module.exports = function () {
+   if(!db) {    
+        db = mysql.createPool({
+                host     : process.env.DB_HOST,
+                user     : process.env.DB_USER,
+                password : process.env.DB_PASSWORD,
+                database : process.env.DB_DATABASE,
+                connectionLimit: 10,
+                supportBigNumbers: true,
+                multipleStatements: true
+       });  
+   }
+   return db;
 };
 
