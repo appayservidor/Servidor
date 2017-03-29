3e13d03afde234aff5f64a8cf62e65cfb16db669
Sergio Pérez <sergperez90@gmail.com>
2017-03-29 09:23:00 +0200

    Ignore

--- helpers/database.js
+++ helpers/database.js
@@ -21,17 +21,19 @@ var mysql = require('mysql');
 var db=null;
 
 module.exports = function () {
-   if(!db) {    
-        db = mysql.createPool({
-                host     : process.env.DB_HOST,
-                user     : process.env.DB_USER,
-                password : process.env.DB_PASSWORD,
-                database : process.env.DB_DATABASE,
-                connectionLimit: 10,
-                supportBigNumbers: true,
-                multipleStatements: true
-       });  
-   }
-   return db;
+    if(!db) {    
+         db = mysql.createPool({
+                 host     : 'localhost',
+                 user     : 'root',
+                 password : 'root',
+                 database : 'appay_heroku',
+                 port: '3306',
+		         socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+                 connectionLimit: 10,
+                 supportBigNumbers: true,
+                 multipleStatements: true
+        });
+    }
+    return db;
 };
 
