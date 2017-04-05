3d3b58daa95de9ec7f0f638fecf8eebabaa370dd
Plr1993 <pablolope93@gmail.com>
2017-04-05 18:41:29 +0200

    Upload final

--- routes/upload.js
+++ routes/upload.js
@@ -8,7 +8,21 @@ var db = require('../helpers/database')();
 router.get('/',function(req,res){
 	db.getConnection(function(err, connection) {
         if (err) throw err;
-		var consulta="UPDATE tienda SET Numero_usuarios_dia_tienda = 0";		
+        var aux = req.query.aux;
+        switch (aux) {
+            case 1:
+                var consulta="UPDATE tienda SET Numero_usuarios_hora_tienda = 0;UPDATE tienda SET Total_ventas_hora_tienda = 0;UPDATE tienda SET Numero_facturas_hora_tienda = 0;UPDATE tienda SET Numero_usuarios_compran_hora_tienda = 0;";		
+                break;
+            case 2:
+                var consulta="UPDATE tienda SET Numero_usuarios_dia_tienda = 0;UPDATE tienda SET Total_ventas_dia_tienda = 0;UPDATE tienda SET Numero_facturas_dia_tienda = 0;UPDATE tienda SET Numero_usuarios_compran_dia_tienda = 0;";	
+                break;
+            case 3:
+                var consulta="UPDATE tienda SET Numero_usuarios_semana_tienda = 0;UPDATE tienda SET Total_ventas_semana_tienda = 0;UPDATE tienda SET Numero_facturas_semana_tienda = 0;UPDATE tienda SET Numero_usuarios_compran_semana_tienda = 0;";	
+                break;
+            case 4:
+                var consulta="UPDATE tienda SET Numero_usuarios_mes_tienda = 0;UPDATE tienda SET Total_ventas_mes_tienda = 0;UPDATE tienda SET Numero_facturas_mes_tienda = 0;UPDATE tienda SET Numero_usuarios_compran_mes_tienda = 0;";	
+                break;
+        }
 		console.log("Consulta:");
 		console.log(consulta);
 		connection.query(consulta,function(err, rows, fields){
@@ -18,7 +32,7 @@ router.get('/',function(req,res){
 			}else{
 				console.log("Query OK");
 				if(rows.length != 0){
-                    console.log("Acgtualizo las estadisticas");
+                    console.log("Actualizo las estadisticas");
 					return res.status(200).json("Ok");
 				}else{
 					console.log("No hay Ofertas...");
