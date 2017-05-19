ddd7b069515bad1f81dba726b571d6c094339925
EMILIO\LenovoPc <emiliomaestre94@gmail.com>
2017-05-19 19:12:06 +0200

    Login Facebook

--- routes/loginjwt.js
+++ routes/loginjwt.js
@@ -38,7 +38,40 @@ router.post("/", function(req,res,next){
         });
     connection.release();
     });
-}); 
+});
+
+//Para Facebook
+router.post("/facebook", function(req,res,next){
+    db.getConnection(function(err, connection) {    
+        if (err) throw err;
+        var username =connection.escape(req.body.username);
+        var password =connection.escape(req.body.password);
+        console.log("username es "+username);
+        console.log("password es "+password);
+        //llamamos a la base de datos para ver si el usuario es correcto o no 
+        var consulta="SELECT * from usuario where Email_usuario="+username;
+        console.log(consulta);
+        connection.query(consulta, function(err, rows, fields) {
+            if(err){
+                console.log(err); 
+                htmlerror(err); 					return res.status(400).json({ error: err });
+            }else{
+                if(rows!=null && rows.length != 0){ //si es correcto
+                    var user=rows[0];
+                    console.log(user);
+                    var token= jwt.sign({//firmamos el token , que caduca en 7 dias
+                        data: user
+                    }, mySecretKey, { expiresIn: '168h' });
+
+                    return res.status(200).json(token);  //lo enviamos
+                }else{
+                    return res.status(401).json("El usuario no existe");
+                }
+            }
+        });
+    connection.release();
+    });
+});  
 //Metodo login para admin
 router.post("/admin", function(req,res,next){
     db.getConnection(function(err, connection) {    
